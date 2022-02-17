0623f7dd314b333ff657b339f85d33400f868709
eugenioclrc <eugenioclrc@gmail.com>
2022-02-17 11:30:53 -0300

    aave interfaces

---
+++ hardhat/contracts/AAVEInterfaces.sol
@@ -0,0 +1,207 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IScaledBalanceToken {
+  /**
+   * @dev Returns the scaled balance of the user. The scaled balance is the sum of all the
+   * updated stored balance divided by the reserve's liquidity index at the moment of the update
+   * @param user The user whose balance is calculated
+   * @return The scaled balance of the user
+   **/
+  function scaledBalanceOf(address user) external view returns (uint256);
+
+  /**
+   * @dev Returns the scaled balance of the user and the scaled total supply.
+   * @param user The address of the user
+   * @return The scaled balance of the user
+   * @return The scaled balance and the scaled total supply
+   **/
+  function getScaledUserBalanceAndSupply(address user) external view returns (uint256, uint256);
+
+  /**
+   * @dev Returns the scaled total supply of the variable debt token. Represents sum(debt/index)
+   * @return The scaled total supply
+   **/
+  function scaledTotalSupply() external view returns (uint256);
+}
+
+interface IWMATIC is IERC20{
+  function deposit() external payable;
+
+  function withdraw(uint256) external;
+}
+
+interface IMATIC {
+  function deposit() external payable;
+
+  function withdraw(uint256) external;
+
+  function approve(address guy, uint256 wad) external returns (bool);
+
+  function transferFrom(
+    address src,
+    address dst,
+    uint256 wad
+  ) external returns (bool);
+
+  function balanceOf(address _owner) external view returns (uint balance);
+  function transfer(address dst, uint wad) external returns (bool);
+
+}
+
+interface IAAVEdataProvider {
+    function getUserReserveData(address asset, address user)
+    external
+    view
+    returns (
+      uint256 currentATokenBalance,
+      uint256 currentStableDebt,
+      uint256 currentVariableDebt,
+      uint256 principalStableDebt,
+      uint256 scaledVariableDebt,
+      uint256 stableBorrowRate,
+      uint256 liquidityRate,
+      uint40 stableRateLastUpdated,
+      bool usageAsCollateralEnabled
+    );
+}
+interface IAToken is IERC20, IScaledBalanceToken {
+
+  /**
+   * @dev Emitted after the mint action
+   * @param from The address performing the mint
+   * @param value The amount being
+   * @param index The new liquidity index of the reserve
+   **/
+  event Mint(address indexed from, uint256 value, uint256 index);
+
+  /**
+   * @dev Mints `amount` aTokens to `user`
+   * @param user The address receiving the minted tokens
+   * @param amount The amount of tokens getting minted
+   * @param index The new liquidity index of the reserve
+   * @return `true` if the the previous balance of the user was 0
+   */
+  function mint(
+    address user,
+    uint256 amount,
+    uint256 index
+  ) external returns (bool);
+
+  /**
+   * @dev Emitted after aTokens are burned
+   * @param from The owner of the aTokens, getting them burned
+   * @param target The address that will receive the underlying
+   * @param value The amount being burned
+   * @param index The new liquidity index of the reserve
+   **/
+  event Burn(address indexed from, address indexed target, uint256 value, uint256 index);
+
+  /**
+   * @dev Emitted during the transfer action
+   * @param from The user whose tokens are being transferred
+   * @param to The recipient
+   * @param value The amount being transferred
+   * @param index The new liquidity index of the reserve
+   **/
+  event BalanceTransfer(address indexed from, address indexed to, uint256 value, uint256 index);
+
+  /**
+   * @dev Burns aTokens from `user` and sends the equivalent amount of underlying to `receiverOfUnderlying`
+   * @param user The owner of the aTokens, getting them burned
+   * @param receiverOfUnderlying The address that will receive the underlying
+   * @param amount The amount being burned
+   * @param index The new liquidity index of the reserve
+   **/
+  function burn(
+    address user,
+    address receiverOfUnderlying,
+    uint256 amount,
+    uint256 index
+  ) external;
+
+
+
+  /**
+   * @dev Mints aTokens to the reserve treasury
+   * @param amount The amount of tokens getting minted
+   * @param index The new liquidity index of the reserve
+   */
+  function mintToTreasury(uint256 amount, uint256 index) external;
+
+  /**
+   * @dev Transfers aTokens in the event of a borrow being liquidated, in case the liquidators reclaims the aToken
+   * @param from The address getting liquidated, current owner of the aTokens
+   * @param to The recipient
+   * @param value The amount of tokens getting transferred
+   **/
+  function transferOnLiquidation(
+    address from,
+    address to,
+    uint256 value
+  ) external;
+
+  /**
+   * @dev Transfers the underlying asset to `target`. Used by the LendingPool to transfer
+   * assets in borrow(), withdraw() and flashLoan()
+   * @param user The recipient of the aTokens
+   * @param amount The amount getting transferred
+   * @return The amount transferred
+   **/
+  function transferUnderlyingTo(address user, uint256 amount) external returns (uint256);
+}
+interface ILendingPoolAddressesProvider {
+    function getLendingPool() external view returns (address);
+}
+
+interface IWETHGateway {
+  function depositETH(
+    address lendingPool,
+    address onBehalfOf,
+    uint16 referralCode
+  ) external payable;
+
+  function withdrawETH(
+    address lendingPool,
+    uint256 amount,
+    address onBehalfOf
+  ) external;
+
+  function repayETH(
+    address lendingPool,
+    uint256 amount,
+    uint256 rateMode,
+    address onBehalfOf
+  ) external payable;
+
+  function borrowETH(
+    address lendingPool,
+    uint256 amount,
+    uint256 interesRateMode,
+    uint16 referralCode
+  ) external;
+
+  function getWETHAddress() external view returns (address);
+}
+
+interface ILendingPool {
+
+    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+
+    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external;
+
+    function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external returns (uint256);
+
+    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+
+    function getUserAccountData(address user) external view returns (
+        uint256 totalCollateralETH,
+        uint256 totalDebtETH,
+        uint256 availableBorrowsETH,
+        uint256 currentLiquidationThreshold,
+        uint256 ltv,
+        uint256 healthFactor
+    );
+}
---
+++ hardhat/contracts/AAVEVault.sol
@@ -0,0 +1,132 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./AAVEInterfaces.sol";
+
+contract Vault is Ownable {
+  IWETHGateway WETHGateway;
+  address LendingPoolAddressesProviderAddress;
+  IAToken aMATIC;
+  IWMATIC wMATIC;
+
+  // mumbai
+  // _WETHGateway 0xee9eE614Ad26963bEc1Bec0D2c92879ae1F209fA
+  // lendingPool 0x178113104fEcbcD7fF8669a0150721e231F0FD4B
+  // aMATIC 0xF45444171435d0aCB08a8af493837eF18e86EE27
+
+  // main net;
+  // WETHGATEWAY 0xbEadf48d62aCC944a06EEaE0A9054A90E5A7dc97
+  // lendingPool 0xd05e3E715d945B59290df0ae8eF85c1BdB684744
+  // aMATIC 0x8dF3aad3a84da6b69A4DA8aeC3eA40d9091B2Ac4
+  constructor(address _WETHGateway, address _LendingPoolAddressesProviderAddress, address _aMATIC /*, address _wMATIC */) {
+    WETHGateway = IWETHGateway(_WETHGateway);
+    LendingPoolAddressesProviderAddress = _LendingPoolAddressesProviderAddress;
+    aMATIC = IAToken(_aMATIC);
+    wMATIC = IWMATIC(WETHGateway.getWETHAddress());
+    aMATIC.approve(address(WETHGateway), type(uint).max);
+    aMATIC.approve(address(this), type(uint).max);
+    wMATIC.approve(address(this), type(uint).max);
+  }
+
+  //https://docs.aave.com/developers/v/2.0/the-core-protocol/protocol-data-provider
+  function Info() public view returns(uint) {
+
+    (
+      ,//uint256 currentATokenBalance,
+      ,//uint256 currentStableDebt,
+      ,//uint256 currentVariableDebt,
+      ,//uint256 principalStableDebt,
+      ,//uint256 scaledVariableDebt,
+      ,//uint256 stableBorrowRate,
+      uint256 liquidityRate,
+      ,//,uint40 stableRateLastUpdated,
+      //,bool usageAsCollateralEnabled
+    ) = IAAVEdataProvider(0xFA3bD19110d986c5e5E9DD5F69362d05035D045B).getUserReserveData(address(wMATIC), address(this));
+
+    return liquidityRate;
+  }
+
+  function release(address releaser, uint256 nftTokenValue) external onlyOwner {
+    uint256 _releaseValue = (nftTokenValue * 90 / 100);
+    //Address.sendValue(payable(releaser), _releaseValue);
+
+    //Take off that amount from AAVE and send to user
+    withdrawAAVE(releaser, _releaseValue);
+
+    // No need to do extra transfer, withdrawAAVE now send wETH to user
+    // Now the release sends back wMATIC (For Now)
+    //require(wMATIC.balanceOf(address(this)) > _releaseValue, "Not enough wMATIC to release");
+    //wMATIC.transfer(releaser, _releaseValue);
+  }
+
+  function withdrawAAVE(address _user, uint _amount) public onlyOwner {
+    IAToken(aMATIC).approve(address(WETHGateway), _amount);
+    
+    // 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889 WMATIC address
+    ILendingPool(seeLendingPool()).withdraw(
+        address(wMATIC),
+        _amount,
+        address(this)
+      );
+
+    wMATIC.withdraw(_amount);
+    Address.sendValue(payable(_user), _amount);
+  }
+
+  function withdrawAAVEwMATIC(address _user, uint _amount) public onlyOwner {
+    IAToken(aMATIC).approve(address(WETHGateway), _amount);
+    
+    // 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889 WMATIC address
+    ILendingPool(seeLendingPool()).withdraw(
+        address(wMATIC),
+        _amount,
+        _user
+      );
+  }
+
+  function depositAAVE() external payable {
+    WETHGateway.depositETH{value: msg.value }(
+        seeLendingPool(),
+        address(this),
+        0
+    );
+  }
+
+  function wrapMATIC() external payable {
+      wMATIC.deposit{value: msg.value}();
+  }
+
+  function unwrapMATIC(uint _amount) external {
+      wMATIC.transfer(address(this),_amount);
+      wMATIC.withdraw(_amount);
+  }
+
+  // function to send rewards upon liquidation
+  function sendMoney(address user, uint256 amount) external onlyOwner {
+    Address.sendValue(payable(user), amount);
+  }
+
+  function seeLendingPool() public view returns (address){
+      return ILendingPoolAddressesProvider(LendingPoolAddressesProviderAddress).getLendingPool();
+  }
+
+  function aMATICbalance(address _addr) public view returns(uint256){
+      return aMATIC.balanceOf(_addr);
+  }
+
+  function wMATICbalance(address _addr) public view returns(uint256){
+      return wMATIC.balanceOf(_addr);
+  }
+  function maticBalance(address _addr) public view returns(uint){
+      return _addr.balance;
+  }
+  receive() external payable {
+    // depositAAVE();
+  }
+}
