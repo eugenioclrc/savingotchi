12a8761b3986591616d69836b0f5c80029877357
eugenioclrc <eugenioclrc@gmail.com>
2022-03-22 16:50:49 -0300

    .

--- web/src/lib/eth.js
+++ web/src/lib/eth.js
@@ -18,7 +18,7 @@ const CHAIN_ID = 80001; // 31337
 // const CHAIN_ID = 31337;
 const HEXCHAIN_ID = "0x"+CHAIN_ID.toString(16);
 
-const savingotchiAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const savingotchiAddress = "0x660c6d095f6d786224d4609D279145038aE78f39";
 let savingotchi;
 
 export const contract = writable(null);
